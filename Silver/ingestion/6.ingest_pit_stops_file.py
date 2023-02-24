ac9b49482a30e4bd1cab526769125d5c7b65b721
Rabbil <rabbil.bhuiyan@helsinki.fi>
2023-02-25 00:01:26 +0200

    update pit_stops file

--- Silver/ingestion/6.ingest_pit_stops_file.py
+++ Silver/ingestion/6.ingest_pit_stops_file.py
@@ -25,12 +25,9 @@ v_file_date = dbutils.widgets.get("p_file_date")
 # MAGIC %md
 # MAGIC ##### Step 1 - Read the JSON file using the spark dataframe reader API
 
-# COMMAND ----------
-
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
-# COMMAND ----------
-
+# specify schema
 pit_stops_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
                                       StructField("driverId", IntegerType(), True),
                                       StructField("stop", StringType(), True),
@@ -40,29 +37,27 @@ pit_stops_schema = StructType(fields=[StructField("raceId", IntegerType(), False
                                       StructField("milliseconds", IntegerType(), True)
                                      ])
 
-# COMMAND ----------
+# reading the data into dataframe
 
 pit_stops_df = spark.read \
 .schema(pit_stops_schema) \
 .option("multiLine", True) \
 .json(f"{raw_folder_path}/{v_file_date}/pit_stops.json")
 
-# COMMAND ----------
+# display the dataframe
+display(pit_stops_df)
 
 # MAGIC %md
 # MAGIC ##### Step 2 - Rename columns and add new columns
-# MAGIC 1. Rename driverId and raceId
-# MAGIC 1. Add ingestion_date with current timestamp
+# Rename driverId and raceId
+# Add ingestion_date with current timestamp
 
-# COMMAND ----------
+# Adding ingestion date using function
 
 pit_stops_with_ingestion_date_df = add_ingestion_date(pit_stops_df)
 
-# COMMAND ----------
 
 from pyspark.sql.functions import lit
-
-# COMMAND ----------
 
 final_df = pit_stops_with_ingestion_date_df.withColumnRenamed("driverId", "driver_id") \
 .withColumnRenamed("raceId", "race_id") \
@@ -70,12 +65,13 @@ final_df = pit_stops_with_ingestion_date_df.withColumnRenamed("driverId", "drive
 .withColumn("data_source", lit(v_data_source)) \
 .withColumn("file_date", lit(v_file_date))
 
-# COMMAND ----------
 
 # MAGIC %md
 # MAGIC ##### Step 3 - Write to output to processed container in parquet format
+final_df.write.mode("overwrite").parquet("/mnt/formula1dl/processed/pit_stops")
 
-# COMMAND ----------
+# check the output is writen properly
+display(spark.read.parquet("/mnt/formula1dl/processed/pit_stops"))
 
 #overwrite_partition(final_df, 'f1_processed', 'pit_stops', 'race_id')
 

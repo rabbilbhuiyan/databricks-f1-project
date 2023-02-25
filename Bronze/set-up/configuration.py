756b04b4c1cd1c7996a30eab9792a3424f08b2ef
Rabbil <rabbil.bhuiyan@helsinki.fi>
2023-02-25 16:07:30 +0200

    edited config file

--- Bronze/set-up/configuration.py
+++ Bronze/set-up/configuration.py
@@ -3,5 +3,4 @@ raw_folder_path = "/mnt/formula1dl/raw"
 processed_folder_path = "/mnt/formula1dl/processed"
 presentation_folder_path = "/mnt/formula1dl/presentation"
 
-# COMMAND ----------
 
--- Bronze/set-up/mount_adls_storage.py
+++ Bronze/set-up/mount_adls_storage.py
@@ -1,60 +1,48 @@
 # Databricks notebook source
+
+# use Azure Key Vault to keep the credentials secret
+# printing the list of secrets from Secret Scope
+dbutils.secrets.list("formula1-scope")
+
+# set-up the variables and replace hard-coded secrets with the dbutils secrets
 storage_account_name = "formula1dl"
 client_id            = dbutils.secrets.get(scope="formula1-scope", key="databricks-app-client-id")
 tenant_id            = dbutils.secrets.get(scope="formula1-scope", key="databricks-app-tenant-id")
 client_secret        = dbutils.secrets.get(scope="formula1-scope", key="databricks-app-client-secret")
 
-# COMMAND ----------
-
+# set-up the config parameters
 configs = {"fs.azure.account.auth.type": "OAuth",
            "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
            "fs.azure.account.oauth2.client.id": f"{client_id}",
            "fs.azure.account.oauth2.client.secret": f"{client_secret}",
            "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
 
-# COMMAND ----------
-
+# mounting the storage in the container
 def mount_adls(container_name):
   dbutils.fs.mount(
     source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
     mount_point = f"/mnt/{storage_account_name}/{container_name}",
     extra_configs = configs)
 
-# COMMAND ----------
-
+# mounting raw conatiner 
 mount_adls("raw")
 
-# COMMAND ----------
-
+# mounting processed conatiner 
 mount_adls("processed")
 
-# COMMAND ----------
-
+# mounting presentation conatiner 
 mount_adls("presentation")
 
-# COMMAND ----------
 
-mount_adls("demo")
-
-# COMMAND ----------
-
-dbutils.fs.unmount("/mnt/formula1dl/demo")
-
-# COMMAND ----------
-
+# check the list of mount points for raw container
 dbutils.fs.ls("/mnt/formula1dl/raw")
+dbutils.fs.mounts()
 
-# COMMAND ----------
-
+# check the list of mount points for processed container
 dbutils.fs.ls("/mnt/formula1dl/processed")
-
-# COMMAND ----------
-
+dbutils.fs.mounts()
+# check the list of mount points for presentation container
 dbutils.fs.ls("/mnt/formula1dl/presentation")
+dbutils.fs.mounts()
 
-# COMMAND ----------
-
-dbutils.fs.ls("/mnt/formula1dl/demo")
-
-# COMMAND ----------
 
